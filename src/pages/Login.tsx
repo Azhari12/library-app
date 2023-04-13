28b4667f04a0e3957e08b3d266cbe1509236cf6e
ysfashidicki <yusuf.1336@students.amikom.ac.id>
2023-04-13 10:26:14 +0700

    slicing: finishing MyBookPage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
-=======
->>>>>>> a3aefaf (slicing: my-book-page)
-=======
->>>>>>> a3aefaf6b19cf111e4c5bc405d76bc33dce038c9
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -41,11 +36,8 @@ const Login = () => {
   };
 
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <Layout>
-      <Navbar />
-      <div className="relative flex flex-col justify-center my-[12%] overflow-hidden">
+      <div className="relative flex flex-col justify-center mt-[12%] overflow-hidden">
         <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-slate-600/40 lg:max-w-xl">
           <h1 className="text-3xl font-extrabold text-center text-green-500">
             Library App
@@ -96,63 +88,6 @@ const Login = () => {
         </div>
       </div>
     </Layout>
-=======
-=======
->>>>>>> a3aefaf6b19cf111e4c5bc405d76bc33dce038c9
-    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-slate-600/40 lg:max-w-xl">
-        <h1 className="text-3xl font-extrabold text-center text-green-500">
-          Library App
-        </h1>
-        <h1 className="text-3xl font-semibold text-left text-black-500">
-          Login
-        </h1>
-        <form className="mt-6" onSubmit={handleLogin}>
-          <div className="mb-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Email Address
-            </label>
-            <input
-              type="email"
-              placeholder="hello@example.com"
-              className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-
-          <div className="mt-6">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-slate-800 rounded-md hover:bg-black focus:outline-none focus:bg-black-600"
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-<<<<<<< HEAD
->>>>>>> a3aefaf (slicing: my-book-page)
-=======
->>>>>>> a3aefaf6b19cf111e4c5bc405d76bc33dce038c9
   );
 };
 
